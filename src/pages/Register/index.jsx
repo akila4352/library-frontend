f6d838e0ec3b498b5af777654112e9861156c0f5
Akila Nirmal <akilanirmalzz4352@gmail.com>
2024-11-21 01:23:49 +0530

    host

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -59,7 +59,7 @@ const Register = () => {
     }
   
     try {
-      const response = await fetch('https://backend-folder.vercel.app/register', {
+      const response = await fetch('http://localhost:5000/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
