649074e8eeb356302dffd230a14b8b32b1d903ef
Akila Nirmal <akilanirmalzz4352@gmail.com>
2024-11-23 00:53:53 +0530

    a

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -59,7 +59,7 @@ const Register = () => {
     }
   
     try {
-      const response = await fetch('https://library2-a07874b48aa0.herokuapp.com/register', {
+      const response = await fetch('http://localhost:5000/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
